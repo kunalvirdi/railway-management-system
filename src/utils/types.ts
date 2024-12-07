fe374ab8661e6c3f2c158fb37107a48b554009a4
Kunal Virdi <kunalvirdi.patiala@gmail.com>
2024-12-07 12:37:26 +0530

    added types

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,6 +1,12 @@
 import {Sequelize} from "sequelize";
+import {z} from "zod";
+import UserSchema from "../schemas/user.schema";
 
 export interface DBType{
     db:Sequelize|null,
     getDB():Sequelize
 }
+export type ErrorType={
+
+}
+export type UserType=z.infer<typeof UserSchema>
